60bdddd3b6fd0c4ae1d35a5a8c3d622fb44fa473
Raymond <109017230+lxd98@users.noreply.github.com>
2023-07-21 18:16:28 +0800

    Add files via upload

--- Giraffe_View/plot.py
+++ Giraffe_View/plot.py
@@ -1,119 +1,106 @@
-import rpy2.robjects as robjects
+from plotnine import * 
+import pandas as pd
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
 
 def plot_observe():
-	rscript='''
-	library(ggplot2)
-	library(reshape2)
-	library(patchwork)
+	df=pd.read_csv("./results/quality/final_observed_accuracy.txt", sep="\t")
+	df=pd.DataFrame(df)
 
-	homo <- function(input_file){
-	  df <- read.csv(input_file, header = T, sep = "\t")
-	  df["group"] <- "test"
-	  p <- ggplot(df, aes(x=base,y=value*100, group=group))  + 
-	    geom_line(alpha=0.5) + geom_point(size=2) +
-	    scale_y_continuous(name="Accuracy of homopolymer identification (%)", 
-	                       limits = c(0,100), breaks = seq(0,100,10)) +
-	    xlab("Base") + theme_bw() + 
-	    theme(axis.text=element_text(size=12, family = "Arial", color="black"),
-	          axis.title=element_text(size=12, family = "Arial", color="black"),
-	          legend.text = element_text(size=12, family = "Arial", color="black"),
-	          legend.title = element_blank(), legend.position = "bottom")
-	  return(p)
-	}
-
-	acc <- function(input_file) {
-	  df <- read.csv(input_file, header = T, sep = "\t") 
-	  p <- ggplot(df, aes(x=Acc*100)) + geom_density(adjust=10, alpha=0.8) +
-	    scale_x_continuous(name="Observed read accuracy (%)", 
-	                       limits = c(0,100), breaks = seq(0,100,10)) + 
-	    scale_y_continuous(name="Probability Density Function (PDF)") + theme_bw() +
-	    theme(axis.text=element_text(size=12, family = "Arial", color="black"),
-	          axis.title=element_text(size=12, family = "Arial", color="black"),
-	          legend.text = element_text(size=12, family = "Arial", color="black"))
-	  return(p)
-	}
-
-	mismatch <- function(input_file){
-	  df <- read.csv(input_file, header = T, sep = "\t")
-	  df["group"] <- "test"
-	  df["p_ins"] <- 100*df["Ins"]/(df["Ins"] + df["Del"] + df["Sub"] + df["Mat"])
-	  df["p_del"] <- 100*df["Del"]/(df["Ins"] + df["Del"] + df["Sub"] + df["Mat"])
-	  df["p_sub"] <- 100*df["Sub"]/(df["Ins"] + df["Del"] + df["Sub"] + df["Mat"])
-	  df1 <- df[, 8:11]
-	  df2 <- melt(df1)
-	  df2$variable <- factor(df2$variable, levels = c("p_ins", "p_del", "p_sub"))
-	  p <- ggplot(df2, aes(x=variable, y=value)) + geom_boxplot(outlier.shape = NA) + theme_bw() +
-	    scale_y_continuous(name="Mismatch proportion (%)", 
-	                       limits = c(0,100), breaks = seq(0,100,10)) +
-	    scale_x_discrete(name="Type", labels=c("Ins", "Del", "Sub")) +
-	    theme(axis.text=element_text(size=12, family = "Arial", color="black"),
-	          axis.title=element_text(size=12, family = "Arial", color="black"),
-	          legend.text = element_text(size=12, family = "Arial", color="black"))
-	  return(p)
-	}
-
-	A <- acc("results/quality/final_observed_accuracy.txt") + 
-		mismatch("results/quality/final_observed_accuracy.txt") + 
-		homo("results/quality/final_homo_summary.txt")
+	df["Acc"] = df["Acc"] * 100
+	accurac_scale = [0,100]
+	accurac_break = [i for i in range(0, 101, 10)]
+	mismatch_scale = [0,21]
+	mismatch_break = [i for i in range(0, 21, 5)]
 	
-	ggsave("observe.png", A, path="./results/quality/", dpi=300, width=12, height=5)
-	'''
-	robjects.r(rscript)
+	Acc = (
+		ggplot(df, aes(x="Acc")) + geom_density(adjust=10, size=1) +
+	    	scale_x_continuous(name="Observed read accuracy", limits=accurac_scale, breaks=accurac_break) +
+	    	scale_y_continuous(name="Probability Density Function (PDF)") + theme_bw() + 
+	   		# geom_vline(aes(xintercept=90), color="#990000", linetype="dashed") +
+	    	# geom_vline(aes(xintercept=99), color="#990000", linetype="dashed") +
+	    	theme(axis_text=element_text(size=12, color="black"),
+	          axis_title=element_text(size=12, color="black"))
+	    )
+
+	df["p_ins"] = 100 * df["Ins"]/(df["Ins"] + df["Del"] + df["Sub"] + df["Mat"])
+	df["p_del"] = 100 * df["Del"]/(df["Ins"] + df["Del"] + df["Sub"] + df["Mat"])
+	df["p_sub"] = 100 * df["Sub"]/(df["Ins"] + df["Del"] + df["Sub"] + df["Mat"])
+	df1 = df[["p_ins", "p_del", "p_sub"]].melt()
+
+	Mis = (
+		ggplot(df1, aes(x="variable", y="value")) + geom_boxplot(outlier_size=0.5) + theme_bw() +
+	    	scale_y_continuous(name="Mismatch proportion (%)", limits=mismatch_scale, breaks=mismatch_break) +
+	    	scale_x_discrete(name="Mismatch type", labels=["Deletion", "Insertion", "Substitution"]) +
+	    	theme(axis_text=element_text(size=12, color="black"),
+	        	  axis_title=element_text(size=12, color="black"))
+		)
+
+	df2=pd.read_csv("./results/quality/final_homo_summary.txt", sep="\t")
+	df2["value"] = df2["value"] * 100
+	df2["group"] = "group"
+
+	Homo = (
+		ggplot(df2, aes(x="base",y="value", group="group"))  + 
+		    geom_line(size=1) + geom_point(size=2) +
+		    scale_y_continuous(name="Accuracy of homopolymer identification (%)", 
+		    	limits=accurac_scale, breaks =accurac_break) +
+		    xlab("Base") + theme_bw() + 
+		    theme(axis_text=element_text(size=12, color="black"),
+		          axis_title=element_text(size=12, color="black"))
+		)
+
+	Acc.save(filename = "Observed_read_accuracy.pdf", width=8, height=6, dpi=300, path="results/quality/")
+	Mis.save(filename = "Observed_mismatch_proportion.pdf", width=8, height=6, dpi=300, path="results/quality/")
+	Homo.save(filename = "Observed_homopolymer_identification.pdf", width=8, height=6, dpi=300, path="results/quality/")
 
 def plot_estimate():
-	rscript = '''
-	library(ggplot2)
-	library(reshape2)
+	df=pd.read_csv("results/estimated/final_estimated_accuracy.txt", sep="\t")
+	df=pd.DataFrame(df)
+	df["acc"] = df["acc"] * 100
+	
+	accurac_scale = [0,100]
+	accurac_break = [i for i in range(0, 101, 10)]
 
-	estimated <- function(input_file){
-	  df <- read.csv(input_file, header = T, sep = "\t")
-	  p <- ggplot(df, aes(x=acc*100)) + geom_density(adjust=10, alpha=0.8) +
-	    scale_x_continuous(name="Estimated read accuracy (%)", 
-	                       limits = c(0,100), breaks = seq(0,100,10)) + 
-	    scale_y_continuous(name="Probability Density Function (PDF)") + theme_bw() +
-	    theme(axis.text=element_text(size=12, family = "Arial", color="black"),
-	          axis.title=element_text(size=12, family = "Arial", color="black"),
-	          legend.text = element_text(size=12, family = "Arial", color="black"))
-	  return(p)
-	}
+	Acc = (
+		ggplot(df, aes(x="acc")) + geom_density(adjust=10, size=1) +
+            scale_x_continuous(name="Estimated read accuracy (%)", limits=accurac_scale, breaks=accurac_break) + 
+            scale_y_continuous(name="Probability Density Function (PDF)") + theme_bw() +
+            theme(axis_text=element_text(size=12, color="black"),
+                  axis_title=element_text(size=12, color="black"))
+		)
 
-	A <- estimated("results/estimated/final_estimated_accuracy.txt")
-	ggsave("estimate.png", A, path="./results/estimated/", dpi=300, width=8, height=5)
-	'''
-	robjects.r(rscript)
+	Acc.save(filename = "estimated_read_accuracy.pdf", width=8, height=6, dpi=300, path="results/estimated")
 
 def plot_GC_bias():
-	rscript = '''
-	library(ggplot2)
-	library(patchwork)
+	df=pd.read_csv("results/GC_bias/GC_bias_raw.txt", sep="\t")
+	df=pd.DataFrame(df)
 
-	bin_count <- function(input_file){
-	  df <- read.csv(input_file, sep = "\t", header = T)
-	  p <- ggplot(df, aes(x=GC_content, y=number)) + geom_point(size=1) + geom_line() +
-	    scale_x_continuous(name = "GC content (%)", 
-	                       limits = c(0,100), breaks = seq(0,100,10)) +
-	    scale_y_continuous(name="Number of bins") + theme_bw() +
-	    theme(axis.text=element_text(size=12, family = "Arial", color="black"),
-	          axis.title=element_text(size=12, family = "Arial", color="black"),
-	          legend.text = element_text(size=12, family = "Arial", color="black"),
-	          legend.title = element_blank(), legend.position = "bottom")
-	  return(p)
-	}
+	accurac_scale = [0,100]
+	accurac_break = [i for i in range(0, 101, 10)]
 
-	GC_bias <- function(input_file){
-	  df <- read.csv(input_file, sep = "\t", header = T)
-	  p <- ggplot(df, aes(x=GC_content, y=nor_dp)) + geom_point(size=1) + geom_line() +
-	    scale_y_continuous(name="Normalized depth", limits = c(0.5,1.5), 
-	                       breaks = seq(0.5,1.5,0.1)) + theme_bw() + 
-	    scale_x_continuous(name="GC content (%)", breaks = seq(0,100,1)) +
-	    theme(axis.text=element_text(size=12, family = "Arial", color="black"),
-	          axis.title=element_text(size=12, family = "Arial", color="black"),
-	          legend.text = element_text(size=12, family = "Arial", color="black"),
-	          legend.title = element_blank(), legend.position = "bottom")
-	  return(p)
-	}
+	distribution_len = (
+		ggplot(df, aes(x="GC_content", y="number")) + geom_point(size=2) + geom_line(size=1) +
+            scale_x_continuous(name = "GC content (%)", limits=accurac_scale, breaks =accurac_break) +
+            scale_y_continuous(name="Number of bins") + theme_bw() +
+            theme(axis_text=element_text(size=12, color="black"),
+                  axis_title=element_text(size=12, color="black"))
+		)
 
-	A <- bin_count("results/GC_bias/GC_bias_raw.txt")/GC_bias("results/GC_bias/final_GC_bias_nor.txt")
-	ggsave("GC_bias.png", A, path="./results/GC_bias/", dpi=300, width=8, height=5)
-	'''
-	robjects.r(rscript)
+	df1=pd.read_csv("results/GC_bias/final_GC_bias_nor.txt", sep="\t")
+	df1=pd.DataFrame(df1)
+
+	GC_bias = (
+		ggplot(df1, aes(x="GC_content", y="nor_dp")) + geom_point(size=2) + geom_line(size=1) +
+			scale_y_continuous(name="Normalized depth", 
+				limits=[0, 2], breaks=np.arange(0, 2.1, 0.2)) +
+			theme_bw() + 
+			scale_x_continuous(name="GC content (%)", breaks=[i for i in range(0, 101, 1)]) +
+            theme(axis_text=element_text(size=12, color="black"),
+                  axis_title=element_text(size=12, color="black"))
+		)
+
+	distribution_len.save(filename = "bin_distribution.pdf", width=8, height=6, dpi=300, path="results/GC_bias/")
+	GC_bias.save(filename = "GC_bias.pdf", width=8, height=6, dpi=300, path="results/GC_bias/")
