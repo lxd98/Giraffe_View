0c16e4406e0466e6cbe9d34a5da12f00fd930bd0
Raymond <109017230+lxd98@users.noreply.github.com>
2024-03-12 16:12:27 +0800

    Add files via upload

---
+++ example/run.sh
@@ -0,0 +1,4 @@
+giraffe estimate --input fastq.list --plot --cpu 4
+giraffe observe --input fastq.list --plot --cpu 4 --ref Read/ecoli_chrom.fa
+giraffe gcbias --input bam.list --plot --ref Read/ecoli_chrom.fa
+giraffe modbin --input bed.list --cpu 4 --plot --bed Methylation/zf_promoter.db
