55095c453951c469119e59d0692b645c06930ead
Raymond <109017230+lxd98@users.noreply.github.com>
2024-03-13 12:29:31 +0800

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,4 +1,4 @@
 giraffe estimate --input fastq.list --plot --cpu 4
 giraffe observe --input fastq.list --plot --cpu 4 --ref Read/ecoli_chrom.fa
 giraffe gcbias --input bam.list --plot --ref Read/ecoli_chrom.fa
-giraffe modbin --input bed.list --cpu 4 --plot --bed Methylation/zf_promoter.db
+giraffe modbin --input bed.list --cpu 4 --plot --pos Methylation/zf_promoter.db
