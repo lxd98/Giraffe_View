64f7e60b03379217f94aba449bb6e334248f868e
Raymond <109017230+lxd98@users.noreply.github.com>
2024-03-13 12:38:34 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="Giraffe_View",
-  version="0.1.0.0",
+  version="0.1.0.1",
   author="Xudong Liu",
   author_email="xudongliu98@gmail.com",
   description="Giraffe_View is specially designed to provide a comprehensive assessment of the accuracy of long-read sequencing datasets obtained from both the PacBio and Nanopore platforms.",
@@ -18,13 +18,14 @@ setuptools.setup(
   "License :: OSI Approved :: MIT License",
   "Operating System :: OS Independent",
   ],
-  python_requires = '>=3.7, <=3.11',
+  python_requires = '>=3.8, <=3.11',
   install_requires=[
   'pysam >= 0.17.0',
   'plotnine >= 0.12.1',
   'numpy >= 1.7.0',
   'pandas >= 1.5.0',
-  'tqdm == 4.64.0'
+  'tqdm == 4.64.0',
+  'termcolor >= 2.0.0'
   ],
   scripts = ["Giraffe_View/giraffe"]
 )
