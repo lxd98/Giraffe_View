448d55875e94463842fb3ea02f008def67188381
Raymond <109017230+lxd98@users.noreply.github.com>
2024-07-29 20:06:11 +0800

    Delete example directory

--- example/run.sh
+++
@@ -1,4 +0,0 @@
-giraffe estimate --input fastq.list --plot --cpu 4
-giraffe observe --input fastq.list --plot --cpu 4 --ref Read/ecoli_chrom.fa
-giraffe gcbias --input bam.list --plot --ref Read/ecoli_chrom.fa
-giraffe modbin --input bed.list --cpu 4 --plot --pos Methylation/zf_promoter.db
